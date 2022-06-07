f75c7d5c21cecf84ffbec2b15a2d40bb345d79d5
x260 <x260@DESKTOP-SDD09U8>
2022-06-07 17:11:43 +0700

    initialize code

---
+++ training.netcorenvc/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace training.netcorenvc.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName ="nvarchar(12)")]
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [Required]
+        public string SWIFTcode { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }   
+}
